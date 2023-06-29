2085055540fbd306d18c5d1351f4ab31aa001d21
Mansi Wani <99387887+MansiWani9@users.noreply.github.com>
2023-06-29 13:44:39 +0530

    Add files via upload

---
+++ Create.java
@@ -0,0 +1,19 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import utilities.SessionFactoryProvider;
+
+public class Create {
+	public static void main(String[] args)
+	{
+		SessionFactory sessionFactory=SessionFactoryProvider.provideSessionFactory();
+		Session session=sessionFactory.openSession();
+		Transaction t=session.beginTransaction();
+		
+		Student s=new Student(101,"John",10);
+		session.save(s);
+		t.commit();
+		
+		sessionFactory.close();
+	}
+}
---
+++ Update.java
@@ -0,0 +1,20 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import utilities.SessionFactoryProvider;
+
+public class Update {
+	public static void main(String[] args)
+	{
+		SessionFactory sessionFactory=SessionFactoryProvider.provideSessionFactory();
+		Session session=sessionFactory.openSession();
+		Transaction t=session.beginTransaction();
+		
+		Student s=session.get(Student.class, 101);
+		s.setStd(11);
+		session.save(s);
+		t.commit();
+		
+		sessionFactory.close();
+	}
+}
